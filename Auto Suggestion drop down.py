b79a2dc030821671fea130d02ea356ebfd46af1e
sushantkadam <sushant.kadam27@gmail.com>
2026-08-16 18:26:45 +0530

    Auto Suggestion Drop Down

---
+++ Auto Suggestion drop down.py
@@ -0,0 +1,27 @@
+import time
+from itertools import dropwhile
+
+from pyexpat.errors import messages
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+driver = webdriver.Chrome()
+#driver.get("http://localhost:3000/login")
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+print(driver.title)
+print(driver.current_url)
+
+driver.find_element(By.ID,"autosuggest").send_keys("ind")
+time.sleep(4)
+
+countries = driver.find_element(By.CSS_SELECTOR, "li[class ='ui-menu-item'] a")
+print(len(countries))
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
