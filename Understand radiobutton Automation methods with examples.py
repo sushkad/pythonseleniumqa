6c1e48b18ab73229098a97ea2c0a725dc39cbec5
sushantkadam <sushant.kadam27@gmail.com>
2026-08-21 23:14:45 +0530

    Understand radiobutton Automation methods with examples

--- Understand radiobutton Automation methods with examples.py
+++ Understand radiobutton Automation methods with examples.py
@@ -9,14 +9,3 @@ driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 print(driver.title)
 print(driver.current_url)
-
-checkboxes = driver.find_elements(By.XPATH,"//input[@type='checkbox']")
-
-print(len(checkboxes))
-
-for checkbox in checkboxes:
-    if checkbox.get_attribute("value") == "option2":
-        checkbox.click()
-        assert checkbox.is_selected()
-
-        break
